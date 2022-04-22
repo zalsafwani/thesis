599e0276d77e15d00e93b43ee2bf737557daed6c
Zahraa Wajih Alsafwani <61522855+zalsafwani@users.noreply.github.com>
2022-04-22 14:48:36 -0500

    Script to visualize PICRUSt  abundance pathway

--- format_pathway_abun.sh
+++ format_pathway_abun.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+module purge
+module load lefse/1.0
+
 cd /work/biocore/zalsafwani/CRC_data/raw_reads/qiime2/second_analysis/picrust/run2/vis-lefse
 
 # Get the path_abun_unstrat.tsv file
@@ -11,9 +14,6 @@ cut ../../../metadata.tsv -f 1,2 > sample_list_type.tsv
 
 # Get the correct format to run LEfSe
 python pathway_format.py -i sample_list_type.tsv -r path_abun_unstrat.tsv -o path_abun_modified.tsv
-
-module purge
-module load lefse/1.0
 
 lefse-format_input.py path_abun_modified.tsv path_abun_modified.in -c 1 -u 2 -o 1000000
 run_lefse.py path_abun_modified.in path_abun_modified.res
